2315ff55b13f11ae08d74d794095162cd0d4a50f
szczepan <259399@student.pwr.edu.pl>
2023-06-03 21:04:25 +0200

    active storage + animal

---
+++ db/migrate/20230603184207_create_animals.rb
@@ -0,0 +1,12 @@
+class CreateAnimals < ActiveRecord::Migration[7.0]
+  def change
+    create_table :animals do |t|
+      t.string :name
+      t.string :breed
+      t.integer :age
+      t.text :enjoys
+
+      t.timestamps
+    end
+  end
+end
