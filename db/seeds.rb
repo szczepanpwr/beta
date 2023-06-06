e2687bea42697e69f8330a5b054746d42ddcdb8a
Szczepan <259399@student.pwr.edu.pl>
2023-06-06 19:44:26 +0200

    seeding admin account

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(email: "admin@mail.com",
+    password: "admin1",
+    password_confirmation: "admin1",
+    role: User.roles[:admin])
